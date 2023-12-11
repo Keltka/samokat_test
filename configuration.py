5f9da73085131761cab7c0c486f682827a497dcd
Keltka <9603553@gmail.com>
2023-12-11 08:13:59 +0300

    samokat ревью 11.12.2023

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://c7fa8cf0-f2ef-49ee-9d9d-614f367b3322.serverhub.praktikum-services.ru"
+BASE_URL = "https://71e35010-6bc6-47c4-9e25-7be9cb6d428b.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER = "/api/v1/orders/track"
